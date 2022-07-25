04057c63fa606431923b7a40165d3354cc2d7931
Angel <82048582+angelangelov88@users.noreply.github.com>
2022-07-25 18:08:29 +0100

    Added function to make the url a string

--- src/App.js
+++ src/App.js
@@ -1,44 +1,56 @@
 import React from 'react';
 import './App.css';
 
-
 function App() {
   const [selectedImage, setSelectedImage] = React.useState(null);
+  const [imgUpload, setImgUpload] = React.useState(null);
+
+  const handleRemove = () => {
+    setSelectedImage(null)
+  }
+
+  const handleUpload = (e) => {
+    console.log(e.target.files[0]);
+    setSelectedImage(e.target.files[0]);
+    var file = e.target.files[0]
+    // console.log(file)
+    let reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => {
+      setImgUpload(reader.result)
+    };
+    console.log(imgUpload);
+    reader.onerror = function (error) {
+      console.log('Error: ', error);
+    }
+  }
 
   return (
     <div>
     <h1>Upload and Display Image usign React Hook's</h1>
-    {selectedImage && (
-      <div>
-      <img alt="not fount" width={"250px"} src={URL.createObjectURL(selectedImage)} />
-      <br />
-      <button onClick={()=>setSelectedImage(null)}>Remove</button>
-      </div>
-    )}
+      {selectedImage && (
+        <div>
+          <img
+            alt="not found"
+            width={"550px"}
+            src={URL.createObjectURL(selectedImage)} 
+          />
+          <br />
+          <button onClick={handleRemove}>Change image</button>
+        </div>
+      )}
     <br />
    
     <br /> 
+    {!selectedImage &&
     <input
       type="file"
-      name="myImage"
-      onChange={(event) => {
-        console.log(event.target.files[0]);
-        setSelectedImage(event.target.files[0]);
-      }}
+      name="imgUpload"
+      accept=".png"
+      onChange={handleUpload}
     />
+    }
   </div>
-
-    // <>
-    //   <div className="App">
-    //     <input 
-    //       // accept="image/*" 
-    //       // id="icon-button-file" 
-    //       type="file" 
-    //       // capture="user"
-    //     />
-
-    //   </div>
-    // </>
   );
 }
 
